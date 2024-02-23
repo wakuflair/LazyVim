57087a9af9c126d2efe01f96ea3ee4862d9c138f
rji1 <rinehart.ji@rockwellautomation.com>
2024-02-23 14:32:51 +0800

    configure telescope-fzf-native

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      layout_strategy = "vertical",
+      layout_config = { prompt_position = "top" },
+      winblend = 0,
+      path_display = { "truncate" },
+    },
+  },
+}
