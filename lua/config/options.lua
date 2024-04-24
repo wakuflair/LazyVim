361a6abe56b0474b23853d564563001a0307fc96
rji1 <rinehart.ji@rockwellautomation.com>
2024-04-24 16:02:47 +0800

    use powershell to lanuch term

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,10 +2,34 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.relativenumber = false
 vim.opt.conceallevel = 0
 vim.opt.title = true
 vim.opt.titlelen = 0
 
+vim.opt.scrolloff = 20
+
 if vim.g.neovide then
   vim.o.guifont = "JetBrainsMono NF:h11"
 end
+
+-- Check if 'pwsh' is executable and set the shell accordingly
+if vim.fn.executable("pwsh") == 1 then
+  vim.o.shell = "pwsh"
+else
+  vim.o.shell = "powershell"
+end
+
+-- Setting shell command flags
+vim.o.shellcmdflag =
+  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+
+-- Setting shell redirection
+vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+
+-- Setting shell pipe
+vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
+
+-- Setting shell quote options
+vim.o.shellquote = ""
+vim.o.shellxquote = ""
